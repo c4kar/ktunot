10e7231f00f97368e9d10665a009830806e61b48
ÇAKAR <ycakar9@gmail.com>
2025-10-21 22:55:24 +0300

    Quartz sync: Oct 21, 2025, 10:55 PM

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -10,7 +10,11 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
     <article class="popover-hint">
       <h1>404</h1>
       <p>{i18n(cfg.locale).pages.error.notFound}</p>
-      <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
+      <a href={baseDir} class="internal">{i18n(cfg.locale).pages.error.home}</a>
+      <blockquote cite="https://www.elise.com/quotes/heinlein_-_specialization_is_for_insects">
+        Bir insan; bebek bezi değiştirebilmeli, bir istilayı planlayabilmeli, bir domuzu kesebilmeli, bir gemiyi idare edebilmeli, bir bina tasarlayabilmeli, bir sone yazabilmeli, hesapları dengeleyebilmeli, bir duvar örebilmeli, bir kemiği yerine oturtabilmeli, ölmekte olana teselli verebilmeli, emir alabilmeli, emir verebilmeli, iş birliği yapabilmeli, tek başına hareket edebilmeli, denklemler çözebilmeli, yeni bir sorunu analiz edebilmeli, gübre saçabilmeli, bir bilgisayar programlayabilmeli, lezzetli bir yemek pişirebilmeli, etkili bir şekilde dövüşebilmeli ve onurlu bir şekilde ölebilmelidir. Uzmanlık böcekler içindir.
+        <footer>— <cite>Robert A. Heinlein</cite></footer>
+      </blockquote>
     </article>
   )
 }
